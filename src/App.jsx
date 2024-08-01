e421a76a1c346d04140885276aeb14b806a68ca5
my name <my name>
2024-08-01 13:48:24 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from "react";
+import "./App.css";
+import "./index.css";
+function App() {
+  const [search, setSearch] = useState("Batman"); // State for search term
+  const [a, setA] = useState([]);
+  const [indexing, setIndexing] = useState(0);
+  const [result, setResult] = useState(false);
+  const url = "https://www.omdbapi.com/?i= &apikey=73aa2f6b";
+
+  async function searching(query) {
+    const response = await fetch(`${url}&s=${search}`);
+    const data = await response.json();
+    setA(data.Search);
+    setResult(true);
+    console.log(data.Search);
+  }
+
+  function increament() {
+    setIndexing(indexing + 1);
+    console.log(indexing);
+  }
+  return (
+    <>
+      <div className="searchbox">
+        {
+          <p>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)} // Update search term
+            />
+            <button onClick={() => searching(search)}>Click here</button>
+            <button onClick={increament}>Next</button>
+          </p>
+        }
+      </div>
+
+      <div className="card-making">
+        {a?.map((item, index) => {
+          console.log(item);
+          console.log("hello");
+          console.log(index);
+          return (
+            <div className="container">
+              <div className="one-div">
+                {console.log(item.Poster)}
+                <img src={item.Poster} alt="image not found"></img>
+              </div>
+              <div className="content">
+                <p>{item.Title}</p>
+                <p>{item.Year}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div style={{margin:100}}>{result ? a[indexing].Poster : "No data"}</div>
+    </>
+  );
+}
+
+export default App;
